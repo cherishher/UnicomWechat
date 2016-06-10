aef64fb982decc9397362c79db2418380ce46129
maxhh <490949611@qq.com>
2016-06-10 12:10:51 +0800

    基本完成

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 LOCAL = u'http://139.196.234.136/wechata'
 
 CARD_URL=u'http://xyk.js165.com:8000/wapycfd//dndx'
+NHCARD_URL=u'http://xyk.js165.com:8000/wapycfd//nuaayjs/'
 party_url = u'http://mp.weixin.qq.com/s?__biz=MzAxMzExMTAwMw==&mid=207384904&idx=1&sn=7554d729670ca80145276b6f64b50312#rd'
 com_url = u'http://mp.weixin.qq.com/s?__biz=MzAxMzExMTAwMw==&mid=207373271&idx=1&sn=6cecb9a46c550d280cb525c3521a4611#rd'
 ResponseContent = u'欢迎关注东大那些年，让我们在未来东大的日子里携手前行，不负青春！'
--- main.py
+++ main.py
@@ -30,7 +30,8 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/wechata/',WechatHandler),
-            (r'/wechata/register/([\S]+)',BindHandler)
+            (r'/wechata/register/([\S]+)',BindHandler),
+            (r'/wechatanh/',WechatHandlera)
             ]
         settings = dict(
             cookie_secret="7CA71A57B571B5AEAC5E64C6042415DE",
@@ -238,7 +239,180 @@ class WechatHandler(tornado.web.RequestHandler):
     #     except NoResultFound:
     #         msg = u'<a href="%s/Tregister/%s">您尚未进行登录注册，点我进行注册登录哦</a>' % (URL, self.wx.openid)
     #         self.write(self.wx.response_text_msg(msg))
-    
+
+
+
+class WechatHandlera(tornado.web.RequestHandler):
+
+    @property
+    def client(self):
+        client = AsyncHTTPClient()
+
+    @property
+    def db(self):
+        return self.application.db
+
+    @property
+    def unitsmap(self):
+        return {
+            'nothing':self.nothing,
+            'unicomCard':self.unicomcard,
+            'tuling':self.tuling_message,
+            'signup':self.signup,
+            'vote':self.vote,
+            'upload':self.upload
+        }
+    def on_finish(self):
+        self.db.close()
+
+    def get(self):
+        self.wx = check.Message(token='Unicom')
+        if self.wx.check_signature(self.get_argument('signature', default=''),
+                                   self.get_argument('timestamp', default=''),
+                                   self.get_argument('nonce', default='')
+                                   ):
+            self.write(self.get_argument('echostr'))
+        else:
+            self.write('access verification fail')
+
+    @tornado.web.asynchronous
+    def post(self):
+        self.wx = check.Message(token='Unicom')
+        if self.wx.check_signature(self.get_argument('signature', default=''),
+                                   self.get_argument('timestamp', default=''),
+                                   self.get_argument('nonce', default='')
+                                   ):
+            self.wx.parse_msg(self.request.body)
+            try:
+                if self.wx.msg_type == 'event' :
+                    if self.wx.event =='subscribe':
+                        self.write(self.wx.response_text_msg(ResponseContent))
+                        self.finish()
+                    elif self.wx.event == 'unsubscribe':
+                        self.write(self.wx.response_text_msg(u'谢谢您的关注'))
+                        self.finish()
+                    elif self.wx.event == 'CLICK':
+                        try:
+                            self.unitsmap[self.wx.event_key]()
+                        except KeyError:
+                            print 'key error'
+                        self.finish()
+                elif self.wx.msg_type == 'text':
+                    self.tuling()
+                    self.finish()
+
+                else:
+                    self.write(self.wx.response_text_msg(u'??'))
+                    self.finish()
+            except:
+                with open('wechat_error.log','a+') as f:
+                    f.write(time.strftime('%Y%m%d %H:%M:%S in [wechat]', time.localtime(time()))+'\n'+str(sys.exc_info()[0])+str(sys.exc_info()[1])+'\n\n')
+                self.write(self.wx.response_text_msg(u'出了点问题T_T,稍后再试吧~'))
+                self.finish()
+        else:
+            self.write('message processing fail')
+            self.finish()
+
+    def nothing(self):
+        # msg = u'您好! 您可以直接回复“提问+您的问题”我们将为您解答，答疑时间为周一至周五09：00-17：00. 东南大学学生事务服务中心联系方式：025-52090282，九龙湖校区大活524。'
+        msg = u' '
+        # self.write(self.wx.response_text_msg(msg))
+    def unicomcard(self):
+        msg =  u'<a href="%s/allweixin">戳我快速领取通信卡！</a>' %NHCARD_URL
+        self.write(self.wx.response_text_msg(msg))
+    def tuling_message(self):
+        msg = u'直接发送消息可进行调戏呦~'
+        self.write(self.wx.response_text_msg(msg))
+
+    def tuling(self):
+        try:
+            msg = u''
+            msg = self.tuling_response(self.wx.raw_content,self.wx.openid)
+            self.write(self.wx.response_text_msg(msg))
+        except Exception,e:
+            msg = u'哦no！人家被玩坏了,等会再调戏人家好不好啊~'
+            self.write(self.wx.response_text_msg(msg))
+
+    def tuling_response(self,info,openid):
+        client = HTTPClient()
+        data = {
+            'info':info.encode('urf-8'),
+            'cardnum':openid
+        }
+        request = HTTPRequest(
+            url = tuling_url,
+            method = 'POST',
+            body = urllib.urlencode(data)
+        )
+        response = client.fetch(request)
+        content = json.loads(response.body)
+        message = json.loads(content['retinfo'])['text']
+        return message
+
+    def signup(self):
+        signupurl  = 'http://unicom.maomengtv.com/unicom/yinc/toapply?from=singlemessage&isappinstalled=0'
+        msg =  u'<a href="%s/allweixin">音超报名</a>' % signupurl
+        self.write(self.wx.response_text_msg(msg))
+
+    def vote(self):
+        voteurl = 'http://unicom.maomengtv.com/unicom/yinc/subject?from=singlemessage&isappinstalled=0'
+        msg =  u'<a href="%s/allweixin">音超投票</a>' % voteurl
+        self.write(self.wx.response_text_msg(msg))
+
+    def upload(self):
+        uploadurl = 'http://unicom.maomengtv.com/unicom/yinc/down?from=singlemessage&isappinstalled=0'
+        msg =  u'<a href="%s/allweixin">音超视频上传</a>' % uploadurl
+        self.write(self.wx.response_text_msg(msg))
+    # def change_pwd(self):
+    #     try:
+    #         teacher = self.db.query(Teacher).filter(Teacher.openid == self.wx.openid).one()
+    #         msg = u'尊敬的%s,您好！<a href="%s/update/%s">点击修改密码</a>' %(teacher.name,URL,self.wx.openid)
+    #         self.write(self.wx.response_text_msg(msg))
+    #     except NoResultFound:
+    #         msg = u'<a href="%s/Tregister/%s">您尚未进行登录注册，点我进行注册登录哦</a>' % (URL, self.wx.openid)
+    #         self.write(self.wx.response_text_msg(msg))
+    # def delete(self):
+    #     try:
+    #         teacher = self.db.query(Teacher).filter(Teacher.openid == self.wx.openid).one()
+    #         teacher.openid = None
+    #         msg = u'尊敬的%s,您好！注销成功' % teacher.name
+    #         self.write(self.wx.response_text_msg(msg))
+    #     except NoResultFound:
+    #         msg = u'<a href="%s/Tregister/%s">您尚未进行登录注册，点我进行注册登录哦</a>' % (URL, self.wx.openid)
+    #         self.write(self.wx.response_text_msg(msg))
+    # def register_infor(self):
+    #     try:
+    #         teacher = self.db.query(Teacher).filter(Teacher.openid == self.wx.openid).one()
+    #         msg = u'尊敬的%s，您好！您的房间号为:%s' % (teacher.name,teacher.room)
+    #         self.write(self.wx.response_text_msg(msg))
+    #     except NoResultFound:
+    #         msg = u'<a href="%s/Tregister/%s">您尚未进行登录注册，点我进行注册登录哦</a>' % (URL, self.wx.openid)
+    #         self.write(self.wx.response_text_msg(msg))
+    # def party_arrangement(self):
+    #     try:
+    #         teacher = self.db.query(Teacher).filter(Teacher.openid == self.wx.openid).one()
+    #         msg = u'尊敬的%s，您好！<a href="%s">点击查看详细会议安排</a>' % (teacher.name,party_url)
+    #         self.write(self.wx.response_text_msg(msg))
+    #     except NoResultFound:
+    #         msg = u'<a href="%s/Tregister/%s">您尚未进行登录注册，点我进行注册登录哦</a>' % (URL, self.wx.openid)
+    #         self.write(self.wx.response_text_msg(msg))
+    # def download(self):
+    #     try:
+    #         teacher = self.db.query(Teacher).filter(Teacher.openid == self.wx.openid).one()
+    #         msg = u'尊敬的%s，您好，<a href="%s/fileList">请点击下载资料</a>' % (teacher.name,URL)
+    #         self.write(self.wx.response_text_msg(msg))
+    #     except NoResultFound:
+    #         msg = u'<a href="%s/Tregister/%s">您尚未进行登录注册，点我进行注册登录哦</a>' % (URL, self.wx.openid)
+    #         self.write(self.wx.response_text_msg(msg))
+    # def communicate(self):
+    #     try:
+    #         teacher = self.db.query(Teacher).filter(Teacher.openid == self.wx.openid).one()
+    #         msg = u'尊敬的%s，您好，<a href="%s">请点击加入交流群</a>' % (teacher.name,com_url)
+    #         self.write(self.wx.response_text_msg(msg))
+    #     except NoResultFound:
+    #         msg = u'<a href="%s/Tregister/%s">您尚未进行登录注册，点我进行注册登录哦</a>' % (URL, self.wx.openid)
+    #         self.write(self.wx.response_text_msg(msg))
+
 
 
 if __name__ == '__main__':
